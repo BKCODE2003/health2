cef30b85a94e68eafc02b8ce713f3fd219266a56
BKCODE2003 <141950104+BKCODE2003@users.noreply.github.com>
2024-05-22 10:50:01 +0530

    Update health.py

--- health.py
+++ health.py
@@ -17,7 +17,7 @@ def get_email(age,gender,height,weight,fitness,exercise,diet):
 
 st.set_page_config(
     page_title="Fitness Plan Generator",
-    page_icon=":barbell:",
+    page_icon="<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="m536-84-56-56 142-142-340-340-142 142-56-56 56-58-56-56 84-84-56-58 56-56 58 56 84-84 56 56 58-56 56 56-142 142 340 340 142-142 56 56-56 58 56 56-84 84 56 58-56 56-58-56-84 84-56-56-58 56Z"/></svg>",
     layout="centered",
     initial_sidebar_state="collapsed" #for side bar initially collapsed
 )
